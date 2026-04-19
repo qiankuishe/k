1c6471ccf5950cb27b0410b39af96c13f621c0a8
miku <miku@mikudeMacBook.local>
2026-04-19 16:58:43 +0800

    feat: v010 - add GitHub Actions auto-build and release

--- gui.py
+++ gui.py
@@ -7,7 +7,7 @@ from window_capture import list_windows
 from region_selector import RegionSelector
 import json
 
-VERSION = "009"
+VERSION = "010"
 
 class MonitorGUI:
     def __init__(self, root):
